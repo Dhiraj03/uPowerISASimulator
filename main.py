1071d4e561c8a284c7f6b3de0d6a86d074228358
Dhiraj <dhirajlokesh@gmail.com>
2020-02-27 01:56:07 +0530

    exec-branch and asm done

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
+#Instructions to be added: 'li' 
 import re
 lr = 0
 ilc = 0
-data_sizes = { "word" : 4,"halfword":2,"byte":1,"asciiz":1}
-registers = {"R1": 0, "R2": 0, "R3": 0, "R4": 0, "R5": 0, "R6": 0}
+data_sizes = { "word" : 4,"halfword":2,"byte":1,"asciiz":1, "doubleword":8}
+reg_values = {"R1": 0, "R2": 3, "R3": -2, "R4": 0, "R5": 0, "R6": 0,"R7":0,"R8":0,"R9":0,"R10":0,"R11":0,"R12":0,"R13":0,"R14":0,"R15":0,"R16":0}
 reg_addresses = {"R1": "00001", "R2": "00010", "R3": "00011", "R4": "00100","R5":"00101","R6":"00110","R7":"00111","R8":"01000","R9":"01001","R10":"01010","R11":"01011","R12":"01100","R13":"01101","R14":"01110","R15":"01111","R16":"10000","R17":"10001","R18":"10010","R19":"10011","R20":"10100","R21":"10101","R22":"10110","R23":"10111","R24":"11000","R25":"11001","R26":"11010","R27":"11011","R28":"11100","R29":"11101","R30":"11110","R31":"11111"}
 data_addr = {}
 data_ctr=0
 symbol_table = {}
 symbol_ctr = 0
+ins = []
+ins_count = 0
 instructions = {"add": {"format": "xo", "op": "011111", "xo": "100001010", "oe": "0", "rc": "0"},
                 "addi": {"format": "d", "op": "001110","exc":0},
-                "addis": {"format": "d", "op": "001111","exc":0},
+                #"addis": {"format": "d", "op": "001111","exc":0},
                 "and": {"format": "x", "op": "011111", "xo": "0000011100","rc":"0"},
                 "andi": {"format": "d", "op": "011100","exc":0},
                 "extsw": {"format": "x", "op": "011111", "rc": "0", "xo": "1111011010"},
@@ -91,6 +94,9 @@ def d(arr):
 def x(arr):
     o = open("machinecode.txt", "a+")
     reg = arr[1].split(',')
+    if arr[0] == 'extsw':
+        o.write(instructions[arr[0]]['op'] + reg_addresses[reg[0]] + reg_addresses[reg[1]] + '00000' + instructions[arr[0]]['xo'] + instructions[arr[0]]['rc'] + '\n')
+        return None
     o.write(instructions[arr[0]]['op'] +
             reg_addresses[reg[0]] + reg_addresses[reg[1]] + reg_addresses[reg[2]] + instructions[arr[0]]['xo'] + instructions[arr[0]]['rc'] + '\n')
 
@@ -196,7 +202,7 @@ def readASM(path):
                 data_addr[line[0]]['value'] = nums
                 data_addr[line[0]]['size'] = len(nums)
             else:                                       # ELSE
-                data_addr[line[0]]['value'] = arr[1].rstrip('\n')
+                data_addr[line[0]]['value'] = arr[2].rstrip('\n')
                 data_addr[line[0]]['size'] = 1
             data_ctr = data_ctr + data_addr[line[0]]['size']*data_sizes[data_addr[line[0]]['type']]   # Maintains the counter in the data section - Similar to the global pointer
         #The loop below parses the text section of the ASM 
@@ -219,6 +225,7 @@ def readASM(path):
             line  = line.lstrip(' ')
             arr = line.split(' ')
             if line[-1] != ":":     #Ensures that it is not a label
+                ins.append(instructions[arr[0]]['format'])
                 if instructions[arr[0]]['format'] == 'xo':
                     xo(arr)
                 elif instructions[arr[0]]['format'] == 'd':
@@ -232,15 +239,241 @@ def readASM(path):
                 elif instructions[arr[0]]['format'] == 'b':
                     b(arr)
     print(lr)
+    print(ins)
 
-                    
+def exec_xo(line):
+    opcode = line[22:31]
+    opcode = (int(opcode, 2))
+    regsrc = [key for key, value in reg_addresses.items() if value ==line[6:11]][0]
+    reg1 = [key for key, value in reg_addresses.items() if value ==line[11:16]][0]
+    reg2 = [key for key, value in reg_addresses.items() if value ==line[16:21]][0]
+    if opcode == 266:
+        reg_values[regsrc] = reg_values[reg1] + reg_values[reg2]
+        print(reg_values[regsrc])
+    else:
+        reg_values[regsrc] = reg_values[reg2] - reg_values[reg1]
+        print(reg_values[regsrc])
+    print(opcode)
+        
+def exec_x(line):
+    opcode = line[21:31]
+    opcode = (int(opcode, 2))
+    regsrc = [key for key, value in reg_addresses.items() if value == line[6:11]][0]
+    reg1 = [key for key, value in reg_addresses.items() if value == line[11:16]][0]
+    if opcode == 986:
+        num1 = reg_values[reg1]
+        if num1 >= 0:
+            num1 = bin(num1)[2:].zfill(32)
+        else:
+            two = 2 ** 32
+            num1 = two + num1
+            num1 = bin(num1)[2:].zfill(32)
+        num2 = ""
+        for i in range(32):
+            num2 += num1[0]
+        reg_values[regsrc] = int(num1, 2)
+        print(reg_values[regsrc])
+        print(num2 + num1)
+    else:
+        reg2 = [key for key, value in reg_addresses.items() if value == line[16:21]][0]
+        two = 2 ** 32
+        num1 = reg_values[reg1]
+        num2 = reg_values[reg2]
+        #The calculations are done in decimal forms - stored in dest registers in decimal forms, but printed as 
+        #binary numbers (2's complement)
+        if opcode == 28:
+            reg_values[regsrc] = num1 ^ num2
+            print(reg_values[regsrc])
+        elif opcode == 476:
+            reg_values[regsrc] = ~(num1 ^ num2)
+            print(reg_values[regsrc])
+        elif opcode == 444:
+            reg_values[regsrc] = num1 | num2
+            print(reg_values[regsrc])
+            
+        elif opcode == 316:
+            reg_values[regsrc] = num1 ^ num2
+            print(reg_values[regsrc])
+        
+        #To convert decimal to 2's complement binary
+        if reg_values[regsrc] >= 0:
+            print(bin(reg_values[regsrc])[2:].zfill(32))
+        else:
+            binary = two + reg_values[regsrc]
+            print(bin(binary)[2:].zfill(32))    
+
+def exec_d(line):
+    opcode = line[0:6]
+    opcode = int(opcode,2)
+    #The first 6 bits are UNIQUE enough to identify the instruction
+    if opcode in (14, 28, 24, 26):
+        two = 2**32
+        regsrc = [key for key, value in reg_addresses.items() if value == line[6:11]][0]
+        reg1 = [key for key, value in reg_addresses.items() if value == line[11:16]][0]
+        print(int(line[16:],2))
+        if line[16] == '0':
+            si = int(line[16:], 2)
+        else:
+            power = 2 ** 16
+            si = (power - int(line[16:], 2)) * -1
+        num1 = reg_values[reg1] 
+        if opcode == 14:
+            reg_values[regsrc] = num1 + si
+        elif opcode == 28:
+            reg_values[regsrc] = num1 & si
+        elif opcode == 24:
+            reg_values[regsrc] = num1 | si
+        elif opcode == 26:
+            reg_values[regsrc] = num1 ^ si
+        
+        print(reg_values[regsrc])
+        if reg_values[regsrc] >= 0:
+            print(bin(reg_values[regsrc])[2:].zfill(32))
+        else:
+            binary = two + reg_values[regsrc]
+            print(bin(binary)[2:].zfill(32))
+    else:
+        two = 2 ** 16
+        reg1 = [key for key, value in reg_addresses.items() if value == line[6:11]][0]
+        reg2 = [key for key, value in reg_addresses.items() if value == line[11:16]][0]
+        if line[16] == '0':
+            offset = int(line[16:], 2)
+        else:
+            offset = (two - int(line[16:], 2)) * (-1)
+        base = reg_values[reg2]
+        addr = base + offset
+        for var in data_addr:
+            if addr >= data_addr[var]['addr'] and addr <= data_addr[var]['addr'] + data_sizes[data_addr[var]['type']] * data_addr[var]['size']:
+                var_name = var
+        #print(var_name)
+        # Implemented only lw/sw, lhz/sth, lbz/stb
+        if opcode == 32:
+            if data_addr[var_name]['size'] == 1:
+                reg_values[reg1] = data_addr[var_name]['value']
+            elif data_addr[var_name]['type'] == 'asciiz':
+                addr = addr - data_addr[var_name]['addr']
+                reg_values[reg1] = data_addr[var_name]['value'][addr] + data_addr[var_name]['value'][addr + 1] + data_addr[var_name]['value'][addr + 2] + data_addr[var_name]['value'][addr + 3]
+            elif data_addr[var_name]['size'] > 1:
+                elts = 4 // (data_sizes[data_addr[var_name]['type']])
+                i = 0
+                reg_values[reg1] =[]
+                while i != elts:
+                    reg_values[reg1].append(data_addr[var_name]['value'][addr + i])
+                    i += 1
+                print(reg_values[reg1])
+        elif opcode == 40 or opcode == 42:
+            if data_addr[var_name]['size'] == 1:
+                reg_values[reg1] = data_addr[var_name]['value']
+            elif data_addr[var_name]['type'] == 'asciiz':
+                addr = addr - data_addr[var_name]['addr']
+                reg_values[reg1] = data_addr[var_name]['value'][addr] + data_addr[var_name]['value'][addr +1] 
+            elif data_addr[var_name]['size'] > 1:
+                elts = 2 // (data_sizes[data_addr[var_name]['type']])
+                i = 0
+                reg_values[reg1] = []
+                while i != elts:
+                    reg_values[reg1].append(data_addr[var_name]['value'][addr + i])
+                    i += 1
+        elif opcode == 34:
+            if data_addr[var_name]['size'] == 1:
+                reg_values[reg1] = data_addr[var_name]['value']
+            elif data_addr[var_name]['type'] == 'asciiz':
+                addr = addr - data_addr[var_name]['addr']
+                reg_values[reg1] = data_addr[var_name]['value'][addr] 
+            elif data_addr[var_name]['size'] > 1:
+                reg_values[reg1] = data_addr[var_name]['value'][addr]
+        elif opcode in (36,37,44,38):
+            data_addr[var_name]['value'] = reg_values[reg1]
+            
+def exec_ds(line):
+    opcode = line[0:6]
+    opcode = int(opcode, 2)
+    two = 2 ** 14
+    reg1 = [key for key, value in reg_addresses.items() if value ==
+            line[6:11]][0]
+    reg2 = [key for key, value in reg_addresses.items() if value ==
+            line[11:16]][0]
+    if line[16] == '0':
+        offset = int(line[16:30], 2)
+    else:
+        offset = (two - int(line[16:30], 2)) * (-1)
+    print(offset)
+    base = reg_values[reg2]
+    addr = base + offset
+    for var in data_addr:
+        if addr >= data_addr[var]['addr'] and addr <= data_addr[var]['addr'] + data_sizes[data_addr[var]['type']] * data_addr[var]['size']:
+            var_name = var
+    if opcode == 58:
+        if data_addr[var_name]['size'] == 1:
+            reg_values[reg1] = data_addr[var_name]['value']
+        elif data_addr[var_name]['type'] == 'asciiz':
+            addr = addr - data_addr[var_name]['addr']
+            for i in range(8):
+                reg_values[var_name] = data_addr[var_name]['value'][addr+i]
+        elif data_addr[var_name]['size'] > 1:
+            addr = addr - data_addr[var_name]['addr']
+            elts = 8 // (data_sizes[data_addr[var_name]['type']])
+            i = 0
+            reg_values[reg1] = []
+            while i != elts:
+                reg_values[reg1].append(data_addr[var_name]['value'][addr + i])
+                i += 1
+            print(reg_values[reg1])
+    elif opcode == 62:
+        data_addr[var_name]['value'] = reg_values[reg1]
+
+
+
+
+
+
+
+
+# def exec_I(line):
+
+# def exec_b(line):
+pc = 0
+
+#The code above is the first part of the assignment, which is ESSENTIALLY CONVERTING ASM CODE TO BINARY
+#The code below is the second part, which is EXECUTION OF BINARY CODE                 
+#print(instructions[])
+def execute(path):
+    global ins_count
+    global pc
+    i = open(path, "r+")
+    for line in i:
+        pc += 1
+        opcode = line[:6]
+        ins_format = ins[ins_count]
+        if ins_format == 'xo':
+            exec_xo(line)
+        elif ins_format == 'x':
+            exec_x(line)
+        elif ins_format == 'd':
+            exec_d(line)
+        elif ins_format == 'ds':
+            exec_ds(line)
+        # elif ins_format == 'I':
+        #     exec_I(line)
+        # elif ins_format == 'b':
+        #     exec_b(line)
+
             
         
-        
+
+
+
 
 readASM("asm.txt")
-print(data_addr)
+execute("machinecode.txt")
+# try:{
+# print(instructions['addi']['rc'])
+# }
+# except: {
+#     print('not found')
+# }
 # print("The symbol table is:")
 # print(symbol_table)
-# print(data_ctr)
+#print(data_ctr)
+print(data_addr)
 # print(ilc)
